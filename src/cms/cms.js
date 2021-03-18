974d4c07db07e077e60102ce02dca05c7d7f79c4
Znaker Studio <79854174+znakerstudio@users.noreply.github.com>
2021-03-18 22:41:51 +0100

    import Omnie Page Preview
    c

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import IndexPagePreview from './preview-templates/OmniePagePreview'
+import OmniePagePreview from './preview-templates/OmniePagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
